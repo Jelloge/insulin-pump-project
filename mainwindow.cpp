840f456de5d3807924d80aabc63f8d8c490bfa8a
Jelloge <elegent.jello@gmail.com>
2025-03-29 17:26:34 -0400

    added core structure; navigation and stacked widget pages

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,11 +6,183 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->OnButton, SIGNAL(clicked()), this, SLOT(turnOn()));
+
+    connect(ui->OffButton, SIGNAL(clicked()), this, SLOT(turnOff()));
+
+    connect(ui->TandemLogo, SIGNAL(clicked()), this, SLOT(returnHomePage()));
+
+    ui->profileTable->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+    connect(ui->ChargeButton, &QPushButton::clicked, this, [this]() {
+        QtConcurrent::run(this, &MainWindow::chargeBattery);
+    });
+
+    connect(ui->UnplugButton, SIGNAL(clicked()), this, SLOT(unplugCharger()));
+
+    connect(ui->OptionsButton, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->OptionsPage);
+    });
+
+    connect(ui->SettingsButton, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->SettingsPage_1);
+    });
+
+    connect(ui->DownButton, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->SettingsPage_2);
+    });
+
+    connect(ui->UpButton, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->SettingsPage_1);
+    });
+
+    connect(ui->BolusButton, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->BolusPage);
+    });
+
+    connect(ui->backToHome, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->HomePage);
+    });
+
+    connect(ui->BTN0, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->BolusPage);
+    });
+
+    connect(ui->enterCarbsBTN, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->enterCarbsPage);
+    });
+
+    connect(ui->backToHome_2, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->HomePage);
+    });
+
+    connect(ui->backToHome_3, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->OptionsPage);
+    });
+
+    connect(ui->backToOptionsBTN, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->OptionsPage);
+    });
+
+    connect(ui->myPumpBTN, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->myPumpPage);
+    });
+
+    connect(ui->backToMyPumpBTN, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->myPumpPage);
+    });
+
+    connect(ui->personalProfilesBTN, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->personalProfilesPage);
+    });
+
+    connect(ui->createProfilePageBTN, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->profileNameCreatePage);
+    });
+
+    connect(ui->addCarbsBTN, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->BolusPage);
+    });
+
+    connect(ui->backToBolusBTN, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->BolusPage);
+    });
+
+    connect(ui->addBgBTN_2, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->BolusPage);
+    });
+
+    connect(ui->addBgBTN, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->enterBgPage);
+    });
+
+    connect(ui->confirmBolusBTN, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->confirmBolusPage);
+    });
+
+    connect(ui->backToDeliverBTN, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->deliverBolusPage);
+    });
+
+    clock = new QTimer(this);
+    connect(clock, &QTimer::timeout, this, &MainWindow::setMainClock);
+    clock->start(1000);
+
+    configData = new ConfigData(ui->stackedWidget->findChild<QWidget*>("SettingsPage_1"), ui->stackedWidget->findChild<QWidget*>("SettingsPage_2"));
+
+    isOn = false;
+    turnOff();
+
+    isCharging = false;
+    battery = 0;
+    ui->BatteryBar->setValue(battery);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete configData;
 }
 
-// Testing github thingy ??
+void MainWindow::turnOff() {
+}
+
+void MainWindow::turnOn() {
+}
+
+bool MainWindow::checkingPIN() {
+}
+
+void MainWindow::returnHome() {
+}
+
+void MainWindow::chargeBattery() {
+}
+
+void MainWindow::chargerUnplugged() {
+}
+
+void MainWindow::batteryDrain() {
+}
+
+void MainWindow::DateTimeChanged(){
+}
+
+void MainWindow::CreateProfileClicked(){
+}
+
+
+void MainWindow::AddCarbsClicked(){
+}
+
+
+void MainWindow::AddBGClicked(){
+}
+
+
+void MainWindow::ConfirmBolusClicked(){
+}
+
+
+void MainWindow::CancelBolusEntry(){
+}
+
+
+
+void MainWindow::ConfirmBolusRejected(){
+}
+
+
+
+void MainWindow::SetDeliverySplitClicked(){
+}
+
+
+void MainWindow::SetDurationClicked(){
+}
+
+
+void MainWindow::SetDeliveryTimeClicked(){
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,16 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtConcurrent/QtConcurrent>
+#include <QThread>
+#include <QWidget>
+#include <QTextEdit>
+#include <QFont>
+#include <QString>
+
+#include "configdata.h"
+
+#define MAX_BATT 100
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +25,50 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+public slots:
+    void turnOff();
+    void turnOn();
+    void returnHome();
+    void chargeBattery();
+    void chargerUnplugged();
+
+private slots:
+
+    void setMainClock();
+
+    void DateTimeChanged(const QDateTime &dateTime);
+
+    void CreateProfileClicked();
+
+    void AddCarbsClicked();
+
+    void AddBGClicked();
+
+    void ConfirmBolusClicked();
+
+    void CancelBolusEntry();
+
+    void ConfirmBolusRejected();
+
+    void SetDeliverySplitClicked();
+
+    void SetDurationClicked();
+
+    void SetDeliveryTimeClicked();
+
+
 private:
     Ui::MainWindow *ui;
+    bool isOn;
+    bool isCharging;
+    int battery;
+    ConfigData *configData;
+    bool existPIN;
+    QTimer *clock;
+    int profNum = 0;
+
+    void batteryDrain();
+    bool checkingPIN();
+    void changeDateTime(const QDateTime &datePlusTime2);
 };
 #endif // MAINWINDOW_H
