3a3f1a4a5ca61ffac778a4b7bc88ce297141df20
LavenJoshi <Lavenjoshi@cmail.carleton.ca>
2025-04-06 03:07:03 -0400

    Added Bolus calculator with storage

---
+++ ESample/mainwindow.h
@@ -0,0 +1,44 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include "BolusInfo.h"
+#include <QMainWindow>
+#include "boluscalculator.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void calculateBolus();  // Slot for calculating bolus
+    void updateBolusDisplay();  // Slot for updating bolus display
+    void addCarbs();  // Slot for adding carbs
+    void cancelBolus();
+    void logBolus(const BolusInfo& info);
+    void deleteHistory();
+
+
+
+
+private:
+    Ui::MainWindow *ui;
+    BolusCalculator bolusCalc;
+    double totalCarbs = 0;  // Variable to accumulate total carbs
+    double immediateBolus = 0;
+    double extendedBolus = 0;
+    double FinalBolus = 0;
+    bool isBolusActive = false; // Track active bolus
+    BolusInfo currentBolusInfo;
+
+};
+
+#endif // MAINWINDOW_H
+
+
+
